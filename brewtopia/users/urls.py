1d0c123f1fa1b9f595928193ca2fc0e7ee0780e5
Nurasyl Artykbaev <hunteradivus@gmail.com>
2023-05-09 01:50:19 +0600

    add base template

--- brewtopia/users/urls.py
+++ brewtopia/users/urls.py
@@ -1,17 +1,9 @@
 from django.urls import path
 from . import views
-from .views import handle_invalid_email
 from .Errors.handlers import handle_not_found, handle_server_error
 
 app_name = 'users'
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('contact/', views.contact_view, name='contact'),
-    path('create_user/', views.create_user, name='create_user'),
 ]
-
-handler404 = handle_not_found
-handler500 = handle_server_error
-handler400 = handle_invalid_email
-
--- brewtopia/users/views.py
+++ brewtopia/users/views.py
@@ -4,46 +4,6 @@ from .forms import UserProfileForm, LoginForm
 
 
 def home_view(request):
-    return render(request, 'users/home.html')
+    return render(request, 'users/base.html')
 
 
-def contact_view(request):
-    return render(request, 'users/Contact.html')
-
-
-def handle_invalid_email(request, exception):
-    return render(request, 'errors/invalid_email.html', {'error_message': str(exception)}, status=400)
-
-
-def create_user(request):
-    if request.method == 'POST':
-        form = UserProfileForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            user = authenticate(request, email=form.cleaned_data['email'], password=form.cleaned_data['password'])
-            login(request, user)
-            return redirect('users:home')
-    else:
-        form = UserProfileForm()
-    return render(request, 'users/create_user.html', {'form': form})
-
-
-def login_view(request):
-    if request.method == 'POST':
-        form = LoginForm(data=request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, email=email, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('users:home')
-            else:
-                form = LoginForm()
-                error_message = 'Invalid login'
-    else:
-        form = LoginForm()
-        error_message = ''
-    return render(request, 'users/login.html', {'form': form, 'error_message': error_message})
